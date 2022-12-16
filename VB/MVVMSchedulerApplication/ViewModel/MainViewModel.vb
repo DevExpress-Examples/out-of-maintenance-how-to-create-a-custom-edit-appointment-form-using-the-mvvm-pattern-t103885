fe0b9ec59d3c8cc82f16842ae9a75502c5cbc23c
DevExpressExampleBot <fakeEmail>
2022-12-16 21:22:38 +0400

    Source auto update [skip ci]

--- VB/MVVMSchedulerApplication/ViewModel/MainViewModel.vb
+++ VB/MVVMSchedulerApplication/ViewModel/MainViewModel.vb
@@ -1,14 +1,16 @@
-﻿#Region "#usings"
-Imports System
-Imports System.Collections.Generic
+#Region "#usings"
 Imports System.ComponentModel
-#End Region ' #usings
 
+#End Region  ' #usings
 Namespace MVVMSchedulerApplication.ViewModel
-    #Region "#mybindinglist"
-    Partial Friend Class MainViewModel
-        Public Property Doctors() As BindingList(Of Doctor)
-        Public Property Appointments() As BindingList(Of HospitalAppointment)
+
+#Region "#mybindinglist"
+    Partial Class MainViewModel
+
+        Public Property Doctors As BindingList(Of Doctor)
+
+        Public Property Appointments As BindingList(Of HospitalAppointment)
+
         Public Sub New()
             Doctors = New BindingList(Of Doctor)()
             Appointments = New BindingList(Of HospitalAppointment)()
@@ -17,20 +19,30 @@ Namespace MVVMSchedulerApplication.ViewModel
         End Sub
 
         Public Class Doctor
-            Public Property Id() As Object
-            Public Property Name() As String
+
+            Public Property Id As Object
+
+            Public Property Name As String
         End Class
 
         Public Class HospitalAppointment
-            Public Property PatientName() As String
-            Public Property Location() As String
-            Public Property StartTime() As Date
-            Public Property EndTime() As Date
-            Public Property InsuranceNumber() As String
-            Public Property FirstVisit() As Boolean
-            Public Property DoctorId() As Object
-            Public Property Notes() As String
+
+            Public Property PatientName As String
+
+            Public Property Location As String
+
+            Public Property StartTime As Date
+
+            Public Property EndTime As Date
+
+            Public Property InsuranceNumber As String
+
+            Public Property FirstVisit As Boolean
+
+            Public Property DoctorId As Object
+
+            Public Property Notes As String
         End Class
     End Class
-    #End Region ' #mybindinglist
+#End Region  ' #mybindinglist
 End Namespace
